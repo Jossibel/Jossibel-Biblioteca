65ea1ebeb66b672e86a9255828af9e5c923f3f9b
Jossibel Perez <jossibel.perez@unl.edu.ec>
2024-05-10 05:06:57 -0500

    Biblioteca

---
+++ src/Biblioteca.java
@@ -0,0 +1,9 @@
+public class Biblioteca {
+    //Atributos
+    public String nombre;
+    public String direccion;
+    //Asociaciones
+    public EstadoBiblioteca estadoBiblioteca;
+    public Repositorio repositorio;
+
+}
